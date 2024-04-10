6bd8cf68d77aabf64fccdfead7a6ad3344f76bae
VISH01AL <g.vish1al@gmail.com>
2024-04-09 22:25:03 -0400

    start of project

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email=models.CharField(unique=True)
+    
 
 # Create your models here.
 class Task(models.Model):
+    fk=models.ForeignKey(User,on_delete=models.CASCADE)
     text=models.CharField(max_length=200)
     completed=models.BooleanField(default=False)
+
